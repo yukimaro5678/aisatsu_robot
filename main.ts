e89feed5f21a2c24f24f309f6f3df53fd28d59d8
yukimaro5678 <70800116+yukimaro5678@users.noreply.github.com>
2021-08-22 10:31:33 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,21 @@
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    if (_1234 == 1) {
+        basic.showString("Hello!")
+    } else if (_1234 == 2) {
+        basic.showString("sayounara")
+    } else if (_1234 == 3) {
+        basic.showString("ohayou")
+    } else {
+        basic.showString("konbanwa")
+    }
+})
 function ちょうおんぱできょりをはかる () {
     pins.digitalWritePin(DigitalPin.P2, 1)
     control.waitMicros(10)
     pins.digitalWritePin(DigitalPin.P2, 0)
     return pins.pulseIn(DigitalPin.P1, PulseValue.High) / 58
 }
+let _1234 = 0
 pins.digitalWritePin(DigitalPin.P2, 0)
 music.playTone(523, music.beat(BeatFraction.Sixteenth))
 basic.pause(500)
@@ -15,28 +27,16 @@ basic.forever(function () {
         basic.pause(500)
         servos.P0.setAngle(180)
         basic.pause(100)
-        music.playTone(392, music.beat(BeatFraction.Whole))
-        music.playTone(349, music.beat(BeatFraction.Whole))
-        music.playTone(330, music.beat(BeatFraction.Double))
-        music.playTone(392, music.beat(BeatFraction.Whole))
-        music.playTone(523, music.beat(BeatFraction.Whole))
-        music.playTone(494, music.beat(BeatFraction.Double))
-        music.playTone(392, music.beat(BeatFraction.Whole))
-        music.playTone(587, music.beat(BeatFraction.Whole))
-        music.playTone(523, music.beat(BeatFraction.Double))
-        music.playTone(659, music.beat(BeatFraction.Breve))
-        music.playTone(523, music.beat(BeatFraction.Whole))
-        music.playTone(494, music.beat(BeatFraction.Whole))
-        music.playTone(440, music.beat(BeatFraction.Breve))
-        music.playTone(698, music.beat(BeatFraction.Whole))
-        music.playTone(587, music.beat(BeatFraction.Whole))
-        music.playTone(523, music.beat(BeatFraction.Double))
-        music.playTone(494, music.beat(BeatFraction.Whole))
-        music.playTone(523, music.beat(BeatFraction.Whole))
-        music.playTone(392, music.beat(BeatFraction.Whole))
-        music.playTone(392, music.beat(BeatFraction.Whole))
-        music.playTone(349, music.beat(BeatFraction.Whole))
-        music.playTone(330, music.beat(BeatFraction.Double))
+        _1234 = randint(1, 3)
+        if (_1234 == 1) {
+            music.playMelody("C D E F G - - - ", 120)
+        } else if (_1234 == 2) {
+            music.playMelody("G F E D C - - - ", 120)
+        } else if (_1234 == 3) {
+            music.playMelody("C D E E D D C - ", 120)
+        } else {
+            music.playMelody("C D E F G - - - ", 120)
+        }
         servos.P0.setAngle(120)
         basic.pause(1000)
         servos.P0.setAngle(180)
